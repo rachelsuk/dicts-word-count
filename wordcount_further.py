91cea96670c23c0445451c442e9eb6c4cecb8545
Rachel <rachelsuk97@gmail.com>
2021-01-31 22:59:06 +0000

    editted wordcount_further.py to use sys.argv and work correctly for twain.txt

--- wordcount_further.py
+++ wordcount_further.py
@@ -1,26 +1,16 @@
 # put your code here
-# word_count = {}
-# file = open('test.txt')
-# my_list = []
-# for line in file:
-#     line = line.rstrip()
-#     my_list[len(my_list):len(my_list)]=line.split(" ")
-# for word in my_list:
-#     if word[-1] in [",", ".", "?"]:
-#         word = word[0:-1]
-#     word_count[word.lower()] =  word_count.get(word.lower(), 0) + 1
-# for word, count in word_count.items():
-#     print(f'{word} {count}')
-
-# the code using counter
-import collections
-word_count = {}
-file = open('test.txt')
-my_list = []
+import sys
+import string
+file = open(sys.argv[1])
+tokens = []
 for line in file:
     line = line.rstrip()
-    my_list[len(my_list):len(my_list)]=line.split(" ")
+    words = line.split()
 
-c = collections.Counter(my_list)
-for word, count in c.items():
-    print(f'{word} {count}')
+    for word in words:
+        word = word.strip(string.punctuation)
+        word = word.lower()
+        tokens.append(word)
+
+print(tokens)
+
