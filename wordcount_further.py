c90bd695b57c4f4dd553850817b3a54406276c0c
Rachel <rachelsuk97@gmail.com>
2021-01-31 23:31:29 +0000

    sort output words by word count

--- wordcount_further.py
+++ wordcount_further.py
@@ -14,8 +14,7 @@ for line in file:
         word = word.lower()
         tokens.append(word)
 word_count = collections.Counter(tokens)
-sorted_word_count = sorted(word_count)
 
-for word in sorted_word_count:
+for word,count in sorted(word_count.items(), key=lambda item: item[1]):
     print(f'{word} {word_count[word]}')
 
