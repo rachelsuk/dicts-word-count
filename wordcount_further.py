93c5a6f7125214518c277d374e7646c930fce473
Rachel <rachelsuk97@gmail.com>
2021-01-31 23:09:35 +0000

    used collections.Counter

--- wordcount_further.py
+++ wordcount_further.py
@@ -1,8 +1,9 @@
 # put your code here
 import sys
 import string
+import collections
 file = open(sys.argv[1])
-word_count = {}
+
 tokens = []
 for line in file:
     line = line.rstrip()
@@ -12,9 +13,8 @@ for line in file:
         word = word.strip(string.punctuation)
         word = word.lower()
         tokens.append(word)
+word_count = collections.Counter(tokens)
 
-for word in tokens:
-    word_count[word] =  word_count.get(word , 0) + 1
 for word, count in word_count.items():
     print(f'{word} {count}')
 
